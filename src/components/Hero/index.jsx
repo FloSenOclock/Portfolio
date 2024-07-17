8970f0d3b21e09ee4f64c8b5d46a57c59ec4776a
FloSenOclock <florian.senor@oclock.school>
2024-07-17 10:50:03 +0200

    mise en page hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,17 +5,17 @@ const Hero = () => {
   return (
     <div className="flex justify-center pt-8">
       <div className=" border-x-2 border-y-8 border-light-border dark:border-dark-border  m-2 max-w-xl flex p-2 text-center ">
-        <div className="max-w-md ">
+        <div>
           <h1 className="text-4xl font-bold">Hello there</h1>
           <img
             className="rounded-full size-28 mx-auto mb-4"
             src="/images/pixelCV.jpg"
             alt="Senor Florian Picture"
           />
-          <p className="py-2 text-left text-sm">
-            Welcome to my portfolio. My name is Senor Florian and i like to
-            build things with
-            <span className="flex items-center">
+          <p className="py-2 text-left text-sm text-center">
+            Welcome to my portfolio. <br />
+            My name is Florian Senor <br /> and i like to build things with{" "}
+            <span className="flex items-center justify-center ">
               React
               <FaReact className="text-xl text-cyan-400 mx-1 animate-[spin_5s_linear_infinite]" />
               and Node.js{" "}
@@ -25,7 +25,7 @@ const Hero = () => {
             <a className="underline" href="mailto:floriansenor@gmail.com">
               contact me
             </a>{" "}
-            if you have any questions{" "}
+            if you have any questions
             <span className="animate-[ping_1.5s_ease-in-out_infinite] font-bold text-2xl">
               .
             </span>
